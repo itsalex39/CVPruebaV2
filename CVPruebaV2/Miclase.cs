80c680632becd6e92bdc2e7716878d90801ab45d
itsalex39 <alexcanor8866@gmail.com>
2026-02-11 09:06:27 +0100

    Estructura de una clase
    Diferentes secciones de la clase con una breve descripción de su funcionalidad

--- CVPruebaV2/Miclase.cs
+++ CVPruebaV2/Miclase.cs
@@ -8,6 +8,18 @@ namespace CVPruebaV2
 {
     internal class Miclase
     {
+        //Constantes - Aspectos de configuracion de la clase
 
+        //Miembros / Campos privados - Agrupar datos protegidos (no accesibles desde fuera de la clase)
+        //Nomenglatura: _nombreCampo
+
+        //Constructores - metodos especiales que definen la forma de instanciar objetos de la clase
+        //Si no se definen constructires, la clase define uno por defecto - MiClase()
+
+        //Propiedades - Interfaz que ofrece la clase para controlar el acceso a los campos protegidos
+        //Implemento los metodos Get-Set para obtener los "datos asociados"
+
+        //Metodos Publicos - Privados
+        
     }
 }
